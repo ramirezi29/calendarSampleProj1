c57e1d1f08a4d1897b6767bf592e760c7f3bb877
Ivan Ramirez <ramirezfranciscoivan@gmail.com>
2019-05-02 11:28:47 -0600

    fist commit

---
+++ calendarSampleProj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  calendarSampleProj
+//
+//  Created by Ivan Ramirez on 5/2/19.
+//  Copyright © 2019 ramcomw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
